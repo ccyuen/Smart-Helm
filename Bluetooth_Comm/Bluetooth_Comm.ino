67946fbfba8465c62643ecf69a7d383b04a1d1ef
alitoyserkani <ali.toyserkani1998@gmail.com>
2017-03-25 16:08:34 -0400

    check to see if app triggers edison

--- Bluetooth_Comm/Bluetooth_Comm.ino
+++ Bluetooth_Comm/Bluetooth_Comm.ino
@@ -1,9 +1,28 @@
+
+//Check later for error with slave
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  Serial1.begin(9600);// ble default baud
+  Serial1.print("AT+CLEAR"); //clear
+  Serial1.print("AT+ROLE0"); // edison as slave
+  Serial.print("AT+SAVE1"); // make sure connection settings are proper
 
 }
 
+char rev;
+
 void loop() {
   // put your main code here, to run repeatedly:
+
+  if (Serial.available()) {
+    rev = Serial.read();
+    Serial1.print(rev);
+  }
+
+  if (Serial1.available()) {
+    rev = Serial1.read();
+    Serial.print(rev);
+  }
 
 }
