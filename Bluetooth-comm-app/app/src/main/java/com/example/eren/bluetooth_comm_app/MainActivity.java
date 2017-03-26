798b458a2497d77b30ca890a08d6d4f7851210d5
eren.rudy <eren@rudy-web.ca>
2017-03-26 00:15:51 -0400

    .equals() added, preventing crashing

--- Bluetooth-comm-app/app/src/main/java/com/example/eren/bluetooth_comm_app/MainActivity.java
+++ Bluetooth-comm-app/app/src/main/java/com/example/eren/bluetooth_comm_app/MainActivity.java
@@ -9,6 +9,7 @@ import java.lang.Object;
         import java.io.OutputStream;
         import android.os.Handler;
 	    import android.os.Message;
+        import java.lang.String;
 
 
 import android.support.v7.app.ActionBarActivity;
@@ -28,12 +29,12 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     public static TextView textViewObj;
-    //BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-   /*     setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
         if (mBluetoothAdapter == null) { // Device does not support Bluetooth
             ((TextView) findViewById(R.id.mainText)).setText("Device Doesn't Support Bluetooth");
@@ -49,14 +50,17 @@ public class MainActivity extends AppCompatActivity {
         String [] pairedList =  new String [pairedDevices.size()];
         int listCounter = 0;
         //String pairedName = "";
+        String pairedAddress = "98:4F:EE:02:DC:9A";
 
         if (pairedDevices.size() > 0) {
 
             ((TextView) findViewById(R.id.mainText)).setText("Paired Devices:");
 
             for (BluetoothDevice device : pairedDevices) {
-                mDevice = device;
 
+                if (device.getAddress().equals(pairedAddress)) {
+                    mDevice = device;
+                }
 
                 pairedList[listCounter] = device.getName();
                 listCounter++;
@@ -66,23 +70,19 @@ public class MainActivity extends AppCompatActivity {
                 ((TextView)findViewById(R.id.pairedList)).append("\n" + pairedList[i]);
             }
 
-            ((TextView)findViewById(R.id.connectText)).append(mDevice.getName());
-
-
-
-        } */
-
-       // ConnectThread mConnectThread = new ConnectThread(mDevice);
-        //mConnectThread.start();
-
-
+            if (mDevice != null) {
+                ((TextView) findViewById(R.id.connectText)).append(mDevice.getName());
+                 ConnectThread mConnectThread = new ConnectThread(mDevice);
+                 mConnectThread.start();
+            }
+        }
     }
-/*
+
     private class ConnectThread extends Thread {
         private final BluetoothSocket mmSocket;
         private final BluetoothDevice mmDevice;
         //private final BluetoothAdapter mBluetoothAdapter = null;
-        private /*static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+        private /*static*/ final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 
         public ConnectThread(BluetoothDevice device) {
             BluetoothSocket tmp = null;
@@ -112,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-   /* private class ConnectedThread extends Thread {
+    private class ConnectedThread extends Thread {
         private final BluetoothSocket mmSocket;
         private final InputStream mmInStream;
         private final OutputStream mmOutStream;
@@ -134,9 +134,12 @@ public class MainActivity extends AppCompatActivity {
             while (true) {
                 try {
                     bytes += mmInStream.read(buffer, bytes, buffer.length - bytes);
+
+                    write(buffer);
+
                     for(int i = begin; i < bytes; i++) {
                         if(buffer[i] == "#".getBytes()[0]) {
-                         //   mHandler.obtainMessage(1, begin, i, buffer).sendToTarget();
+                            mHandler.obtainMessage(1, begin, i, buffer).sendToTarget();
                             begin = i + 1;
                             if(i == bytes - 1) {
                                 bytes = 0;
@@ -159,9 +162,9 @@ public class MainActivity extends AppCompatActivity {
                 mmSocket.close();
             } catch (IOException e) { }
         }
-    } */
+    }
 
-   /* Handler mHandler = new Handler() {
+    Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             byte[] writeBuf = (byte[]) msg.obj;
@@ -175,6 +178,6 @@ public class MainActivity extends AppCompatActivity {
                     break;
             }
         }
-    }; */
+    };
 
 }
--- serialRead_test/serialRead_test.ino
+++ serialRead_test/serialRead_test.ino
@@ -1,27 +1,28 @@
-void initializeBluetooth() {
+/*void initializeBluetooth() {
   Serial1.begin(9600); // ble def. baud
   Serial1.print("AT+CLEAR"); // clear
   Serial1.print("AT+ROLE0"); //edison as slave
   Serial.print("AT+SAVE1"); // proper connection ensurance
-}
+}*/
+
 void listenForBleData() { // updates bluetooth string
   char bData;
   
   if (Serial.available()) {
       bData = Serial.read();
-      Serial1.print(bData);
-  }
-
-  if (Serial1.available()) {
-      bData = Serial1.read();
       Serial.print(bData);
   }
+
+/*  if (Serial1.available()) {
+      bData = Serial1.read();
+      Serial1.print(bData);
+  }*/
 }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); 
-  initializeBluetooth();
+ // initializeBluetooth();
 
 }
 
